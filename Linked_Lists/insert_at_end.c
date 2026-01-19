6035c1de8d17be41a9d75bb6925b9beca94d314b
ShambhaviSeth0403 <sethshambhavi45@gmail.com>
2026-01-19 21:20:27 +0530

    Add insert_at_end.c for inserting nodes in linked list

---
+++ Linked_Lists/insert_at_end.c
@@ -0,0 +1,41 @@
+Q3. Insert a node at the end of a singly linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+int main() {
+    struct node *head = NULL, *newnode, *temp;
+    int choice = 1;
+
+    while(choice) {
+        newnode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter data: ");
+        scanf("%d", &newnode->data);
+        newnode->next = NULL;
+
+        if(head == NULL) {
+            head = temp = newnode;
+        } else {
+            temp->next = newnode;
+            temp = newnode;
+        }
+
+        printf("Insert more nodes? (1/0): ");
+        scanf("%d", &choice);
+    }
+
+    temp = head;
+    printf("Linked List: ");
+    while(temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL");
+
+    return 0;
+}
